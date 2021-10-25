5d334d050fe577ef12f164bb5507692fd9cf1eff
Kseniia Demina <Kseniia@kseniia-c02wv1hlhtdd.lan>
2021-10-25 16:24:07 -0400

    finished cake files and html version of oddcalc

---
+++ cake.py
@@ -0,0 +1,7 @@
+answer = input("Do you want some cake? Yes or no? ")
+if answer == "yes" or answer == "Yes" or answer == "YES" or answer == "y" or answer == "Y":
+    print("Here you go! :)")
+elif answer == "no" or answer == "No" or answer == "NO" or answer == "n" or answer == "N":
+    print("The cake shall remain in my hands... ")
+else:
+    print("I told you to say yes or no!! >:( ")
